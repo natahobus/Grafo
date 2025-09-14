fdd66b3e0626bd8b1bba5e48f1c3cc96fb41dea5
Natã Hobus <natahobus.dev@gmail.com>
2025-09-13 22:32:15 -0300

    Implementação do Algoritmo Dijkstra

---
+++ algoritmos/dijkstra.js
@@ -0,0 +1,37 @@
+function dijkstra(grafo, inicio, objetivo) {
+  let dist = {};
+  let prev = {};
+  let naoVisitados = new Set(Object.keys(grafo));
+
+  for (let no in grafo) {
+    dist[no] = Infinity;
+    prev[no] = null;
+  }
+  dist[inicio] = 0;
+
+  while (naoVisitados.size > 0) {
+    let u = [...naoVisitados].reduce((a, b) => dist[a] < dist[b] ? a : b);
+    naoVisitados.delete(u);
+
+    if (u === objetivo) break;
+
+    for (let vizinho in grafo[u]) {
+      let alt = dist[u] + grafo[u][vizinho];
+      if (alt < dist[vizinho]) {
+        dist[vizinho] = alt;
+        prev[vizinho] = u;
+      }
+    }
+  }
+
+  let caminho = [];
+  let u = objetivo;
+  while (u) {
+    caminho.unshift(u);
+    u = prev[u];
+  }
+
+  return { caminho, custo: dist[objetivo] };
+}
+
+module.exports = dijkstra;
