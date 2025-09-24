5be7eec4004eefe8da55b1e5f865836bdb2bbaf7
Natã Hobus <natahobus.dev@gmail.com>
2025-09-24 18:50:55 -0300

    Adiconando Bfs em Typescript

--- typescript/bfs.ts
+++ typescript/bfs.ts
@@ -1,38 +1,36 @@
 type Grafo = { [cidade: string]: { [vizinho: string]: number } };
 type Resultado = { caminho: string[]; custo: number; visitados: number };
 
-function depthFirst(
-  grafo: Grafo,
-  inicio: string,
-  objetivo: string,
-  visitados: Set<string> = new Set(),
-  caminho: string[] = [],
-  custo: number = 0
-): Resultado {
-  caminho.push(inicio);
-  visitados.add(inicio);
+function breadthFirst(grafo: Grafo, inicio: string, objetivo: string): Resultado {
+  const fila: [string, string[]][] = [[inicio, [inicio]]];
+  const visitados: Set<string> = new Set();
 
-  if (inicio === objetivo) {
-    return { caminho: [...caminho], custo, visitados: visitados.size };
-  }
+  while (fila.length > 0) {
+    const [no, caminho] = fila.shift()!;
 
-  for (const [vizinho, peso] of Object.entries(grafo[inicio] || {})) {
-    if (!visitados.has(vizinho)) {
-      const result = depthFirst(grafo, vizinho, objetivo, visitados, caminho, custo + peso);
-      if (result.caminho.length > 0) return result;
+    if (visitados.has(no)) continue;
+    visitados.add(no);
+
+    if (no === objetivo) {
+      return { caminho, custo: caminho.length - 1, visitados: visitados.size };
+    }
+
+    for (const vizinho of Object.keys(grafo[no] || {})) {
+      if (!visitados.has(vizinho)) {
+        fila.push([vizinho, [...caminho, vizinho]]);
+      }
     }
   }
 
-  caminho.pop(); // backtracking
   return { caminho: [], custo: 0, visitados: visitados.size };
 }
 
 // ----------------- TESTE -----------------
-const graphDFS: Grafo = {
+const graphBFS: Grafo = {
   "Pelotas": { "Camaquã": 150, "Rio Grande": 55 },
   "Camaquã": { "Pelotas": 150, "Porto Alegre": 125 },
   "Rio Grande": { "Pelotas": 55, "Porto Alegre": 320 },
   "Porto Alegre": { "Camaquã": 125, "Rio Grande": 320 }
 };
 
-console.log(depthFirst(graphDFS, "Pelotas", "Porto Alegre"));
+console.log(breadthFirst(graphBFS, "Pelotas", "Porto Alegre"));
